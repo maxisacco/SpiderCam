f0fcfcb7985fe8867bac805282cde357b46d7baa
Maximiliano A. Sacco <msacco@gmail.com>
2014-03-10 18:19:25 -0300

    Renombrar El proyecto a ModuloTractor

---
+++ SpiderCam/ModuloTractor.h
@@ -0,0 +1,33 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef ModuloTractor_H_
+#define ModuloTractor_H_
+#ifndef ARDUINO_H_
+#define ARDUINO_H_
+#include "Arduino.h"
+#endif //ARDUINO_H_
+//add your includes for the project ModuloTractor here
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void run();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project ModuloTractor here
+
+
+
+
+//Do not add code below this line
+#endif /* ModuloTractor_H_ */
