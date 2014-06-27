3ece9c7eda0569b428833b2572c4e993ce8593e5
Maximiliano A. Sacco <msacco@gmail.com>
2014-06-26 21:18:10 -0300

    actualizo
    varios errores

---
+++ SpiderCam/Release/ModuloTractor.cpp.d
@@ -0,0 +1,50 @@
+ModuloTractor.cpp.d ModuloTractor.cpp.o: ../ModuloTractor.cpp \
+  ../ModuloTractor.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Arduino.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/binary.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/WCharacter.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/WString.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/HardwareSerial.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Stream.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Print.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Printable.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/new.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/variants/standard/pins_arduino.h \
+  ../stepperMotor.h ../comunicador.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/HardwareSerial.h \
+  ../traccionador.h ../optoWheel.h \
+  /Users/msacco/Desktop/Arduino/TimerOne/TimerOne.h
+
+../ModuloTractor.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Arduino.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/binary.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/WCharacter.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/WString.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/HardwareSerial.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Stream.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Print.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Printable.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/new.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/variants/standard/pins_arduino.h:
+
+../stepperMotor.h:
+
+../comunicador.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/HardwareSerial.h:
+
+../traccionador.h:
+
+../optoWheel.h:
+
+/Users/msacco/Desktop/Arduino/TimerOne/TimerOne.h:
---
+++ SpiderCam/Release/comunicador.cpp.d
@@ -0,0 +1,21 @@
+comunicador.cpp.d comunicador.cpp.o: ../comunicador.cpp ../comunicador.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/HardwareSerial.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Stream.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Print.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/WString.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Printable.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/new.h
+
+../comunicador.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/HardwareSerial.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Stream.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Print.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/WString.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/Printable.h:
+
+/Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/avr/cores/arduino/new.h:
